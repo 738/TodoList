6a3d91771156582a36b30cca8ed3752702473840
holykw <jydrogen@naver.com>
2017-11-06 20:31:35 +0900

    add actions

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import * as types from './actionTypes';
+
+export function addTodo(text){
+  return {
+    type: types.ADD_TODO,
+    text
+  }
+}
+
+export function toggleTodo(index){
+  return {
+    type: types.TOGGLE_TODO,
+    index
+  }
+}
+
+export function removeTodo(index){
+  return {
+    type: types.REMOVE_TODO,
+    index
+  }
+}
+
+export function removeTodoCompleted(){
+  return {
+    type: types.REMOVE_TODO_COMPLETED
+  }
+}
